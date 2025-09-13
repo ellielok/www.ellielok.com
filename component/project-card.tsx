3eebe241bac9723ae88b3e7c4ae62d8a9d16a740
ellielok <ellielo2021@gmail.com>
2025-09-13 20:15:04 +1000

    Update fonts and layout

--- component/project-card.tsx
+++ component/project-card.tsx
@@ -16,10 +16,10 @@ export default function ProjectCard({
     skills
 }: ProjectCardProps){
     return(<div className="md:flex m-2 flex-row my-10">
-          <div className="basis-1/4 text-sm font-semibold">{timeRange}</div>
+          <div className="basis-1/4 text-sm pb-2 font-semibold">{timeRange}</div>
           <div className='md:px-5 basis-3/3'>
-            <div className="font-bold text-white/590 ">{project}</div>
-            <div className="font-semibold pb-6"> {position}, {company} </div>
+            <div className="font-bold text-white/90 ">{project}</div>
+            <div className="font-semibold pb-2 text-white/60"> {position}, {company} </div>
             <div className="font-base"> {description} </div>
             {skills && <div className="font-base py-2">Skills: {skills}</div>}
           </div>
