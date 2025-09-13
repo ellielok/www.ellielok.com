687c22fc5d340acc2ee5c6a68e42db7ecc1924c4
ellielok <ellielo2021@gmail.com>
2025-09-13 14:01:03 +1000

    Add padding for project columns

--- component/project-card.tsx
+++ component/project-card.tsx
@@ -17,7 +17,7 @@ export default function ProjectCard({
 }: ProjectCardProps){
     return(<div className="flex m-2 flex-row my-10">
           <div className="basis-1/4 text-sm font-semibold ">{timeRange}</div>
-          <div className='basis-3/3 '>
+          <div className='px-5 basis-3/3 '>
             <div className="font-bold text-white/590">{project}</div>
             <div className="font-semibold"> {position}, {company} </div>
             <div className="font-base"> {description} </div>
