0c6a38be8e743bbd240fbc539fcce29648a636e1
ellielok <ellielo2021@gmail.com>
2025-10-27 00:21:46 +1100

    Revise layout of experience cards

--- component/sub-component/project-card.tsx
+++ component/sub-component/project-card.tsx
@@ -20,8 +20,8 @@ export default function ProjectCard({
                           group
                           hover:bg-gradient-to-b hover:from-white/5 hover:via-white/3 hover:to-white/0
                         ">
-          <div className="basis-1/4 text-tiny pb-2 text-tiny/6 font-semibold">{timeRange}</div>
-          <div className='md:px-5 basis-3/3'>
+          <div className="basis-1/5 text-tiny pb-2 text-tiny/6 font-semibold">{timeRange}</div>
+          <div className='md:px-5 basis-4/5'>
             <div className="font-bold text-white/90 group-hover:text-[#93f5fa] transition-colors duration-100">
               {project}
             </div>
