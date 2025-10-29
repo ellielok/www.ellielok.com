d1e82fe7f41a99f59f89b14ec099ff38bf497023
ellielok <ellielo2021@gmail.com>
2025-10-29 23:50:27 +1100

    Fix naming issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 import { Particles } from '@/components/ui/shadcn-io/particles';
 import HalloweenIconToggle from '@/components/HalloweenIconToggle';
 import Header from '@/component/header';
-import BackgroundWrapper from '@/component/background-Wrapper';
+import BackgroundWrapper from '@/component/background-wrapper';
 import FooterWrapper from '@/component/env-footerWrapper';
 
 const inter = Inter({
