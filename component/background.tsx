c2353050a3517423ff94bc6aace918bb43744e4b
ellielok <ellielo2021@gmail.com>
2025-10-30 17:25:31 +1100

    Optimize home page layout

--- component/background.tsx
+++ component/background.tsx
@@ -3,18 +3,16 @@ import Image from 'next/image';
 export default function Background() {
   return (
     <div>
-      {/* Group these images into a <Background> component. It would be easier to manage it and address the "z-level" height in the future development.
-       */}
       <Image
         src="/halloween-moon-light.png"
         alt="Halloween background"
         width={600}
         height={600}
         className="
-            fixed left-1/8 bottom-[0%] 
+            fixed left-1/8 bottom-[10%] 
             translate-y-0
             hidden sm:hidden lg:block
-            w-auto h-1/2
+            w-auto h-2/5
             opacity-100
             pointer-events-none select-none
             object-contain
@@ -28,9 +26,9 @@ export default function Background() {
         width={600}
         height={600}
         className="
-            fixed bottom-0 left-0 translate-y-1
+            fixed bottom-0 left-0 translate-y-0
             hidden sm:hidden lg:block
-            h-1/2 w-auto
+            h-auto w-2/5
             opacity-85
             pointer-events-none select-none
             object-contain
@@ -38,21 +36,6 @@ export default function Background() {
           "
       />
 
-      {/* <Image
-        src="/halloween-mount.png"
-        alt="Halloween background"
-        width={600}
-        height={600}
-        className="
-            fixed bottom-0 left-0 top-[63%]
-            hidden sm:hidden lg:block
-            h-1/2 w-auto
-            opacity-100
-            pointer-events-none select-none
-            object-contain
-            -z-15
-          "
-      /> */}
 
       <Image
         src="/halloween-witch.png"
@@ -60,49 +43,15 @@ export default function Background() {
         width={600}
         height={600}
         className="
-            fixed left-1/4 bottom-[5%]
+            fixed left-1/5 bottom-[10%]
             hidden sm:hidden lg:block
-            h-1/2 w-auto
+            h-2/5 w-auto
             opacity-100
             pointer-events-none select-none
             object-contain
             z-1
           "
       />
-
-      {/* <Image
-          src="/halloween-cat.png"
-          alt="Halloween background"
-          width={600}
-          height={600}
-          className="
-            fixed right-12 bottom-[0%] 
-            translate-y-2
-            hidden sm:hidden lg:block
-            w-1/20 h-auto
-            opacity-100
-            pointer-events-none select-none
-            object-contain
-            -z-20
-          "
-        /> */}
-
-      {/* <Image
-          src="/halloween-light.png"
-          alt="Halloween background"
-          width={600}
-          height={600}
-          className="
-            fixed right-2 top-[0%] 
-            translate-y-0
-            hidden sm:hidden lg:block
-            w-1/10 h-auto
-            opacity-100
-            pointer-events-none select-none
-            object-contain
-            -z-20
-          "
-        /> */}
     </div>
   );
 }
