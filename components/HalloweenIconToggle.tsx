949b0c9416e66cb5290552bdd65894ae04710eea
ellielok <ellielo2021@gmail.com>
2025-10-30 23:32:55 +1100

    Update widget for small size display

--- components/HalloweenIconToggle.tsx
+++ components/HalloweenIconToggle.tsx
@@ -16,6 +16,33 @@ export default function HalloweenIconToggle() {
 }, []);
 
   return (
+    <div>
+<div className="fixed md:hidden right-0 bottom-0 mb-10 mr-10">
+  <motion.div
+    whileTap={{ scale: 0.88 }} // 点击时轻微缩放反馈
+    onClick={() => {
+      if (window.scrollY === 0) {
+        // 当前在首屏 → 滚动到底部
+        window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+      } else {
+        // 否则 → 滚动到顶部
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }
+      setShowIcons(!showIcons);
+    }}
+    className="cursor-pointer select-none"
+  >
+    <Image
+      src="/halloween-light-sm.png"
+      alt="Halloween light"
+      width={120}
+      height={120}
+      className="w-15 h-auto pointer-events-auto z-30"
+    />
+  </motion.div>
+</div>
+
+
     <div className="hidden md:fixed right-6 top-4 md:right-10 md:top-6 md:flex md:flex-col md:items-center z-25">
       <motion.div
   whileTap={{ y: 5, scale: 0.99 }}
@@ -41,7 +68,7 @@ export default function HalloweenIconToggle() {
           className="w-7/10 2xl:w-10/10 h-auto md:-translate-y-55 md:translate-x-20 
           2xl:translate-x-0
           2xl:-translate-y-75
-          pointer-events-auto"
+          pointer-events-auto z-30"
         />
       </motion.div>
 
@@ -57,6 +84,7 @@ export default function HalloweenIconToggle() {
           </motion.div>
         )}
       </AnimatePresence>
+    </div>
     </div>
   );
 }
