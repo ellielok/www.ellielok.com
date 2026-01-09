0eb50a6c00bbd281b7ceb66b2d10440ecbbc4776
ellielok <ellielo2021@gmail.com>
2026-01-09 17:07:12 +1100

    Basic footer & header

---
+++ app/component/header.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
+export default function Menu() {
+  return (
+    <div className="mx-auto max-w-7xl px-4 border border-black/10 ">
+        <div className="flex h-14 items-center justify-between border" >
+      <div className="flex justify-between items-center border"></div>
+      <div className='flex justify-between items-center border'></div>
+      </div>
+    </div>
+  );
+}
