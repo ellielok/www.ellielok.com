1f4d2eb65e73a8d47ccd849004e251560be3f782
ellielok <ellielo2021@gmail.com>
2026-01-09 19:00:12 +1100

    Basic theme toggle

---
+++ app/component/theme-toggle.tsx
@@ -0,0 +1,24 @@
+  'use client'
+
+function ThemeToggle() {
+    return <button onClick={ToggleTheme}>Theme Toggle</button>;}
+
+
+
+function ToggleTheme() {
+    if (!document.startViewTransition) {
+        // Fallback for browsers that do not support View Transitions
+        DarkTheme();
+        return;
+    }
+
+    document.startViewTransition(() => {
+        DarkTheme();
+    });
+}
+
+function DarkTheme() {
+    document.documentElement.classList.add('dark');
+}
+
+export default ThemeToggle;
