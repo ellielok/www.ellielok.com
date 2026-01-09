048d73548a8b8eccea85a75283c35379977dd2b5
ellielok <ellielo2021@gmail.com>
2026-01-09 17:36:16 +1100

    Update layout background

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -7,10 +7,13 @@ export default function MainLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
+    <div
+      className="min-h-screen bg-cover bg-center bg-no-repeat bg-fixed"
+      style={{ backgroundImage: 'url(/images/bg-light.png)' }}
+    >
       <Header />
       {children}
-      <Footer />
-    </>
+      
+    </div>
   );
 }
